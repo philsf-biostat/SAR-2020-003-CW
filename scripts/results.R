f6ec80368c91da5108c4503419e9a75147d5b2d4
Felipe Figueiredo <philsf79@gmail.com>
2020-08-12 15:15:58 -0300

    anova in pander

--- scripts/results.R
+++ scripts/results.R
@@ -1,6 +1,9 @@
 source("scripts/input.R")
+library(pander)
 
 model.treat.t98strain <- aov(Absorbance ~ Treatment + Strain, data = df)
 
 summary(model.treat.t98strain)
+# pander(model.treat.t98strain)
+
 TukeyHSD(model.treat.t98strain)
