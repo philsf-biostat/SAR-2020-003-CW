9543c7d430e45d7b014f5c72496738beb4ad6dd3
Felipe Figueiredo <philsf79@gmail.com>
2020-08-12 15:05:18 -0300

    split factors and filter T98

--- scripts/input.R
+++ scripts/input.R
@@ -1,9 +1,16 @@
 library(readxl)
 library(data.table)
+
 df <- read_excel("dataset/MTT-29-July-20-graph.xls", 
                  range = "A1:D85")
 df <- data.table(df)
 names(df) <- c("Well", "Treatment", "Type", "Absorbance")
+
+# split type column
+df[, c("CLine", "Strain") := tstrsplit(Type, "-", fixed = TRUE)]
+
+# filter only t98 cell line
+df <- df[CLine == "T98G"]
 
 df$Well <- factor(df$Well)
 
@@ -12,3 +19,7 @@ df$Treatment <- relevel(df$Treatment, "No Treatment")
 
 df$Type <- factor(df$Type)
 df$Type <- relevel(df$Type, "T98G-WT")
+
+df$CLine <- factor(df$CLine)
+df$Strain <- factor(df$Strain)
+df$Strain <- relevel(df$Strain, "WT")
