2209047de92f025e0357e93296a5b62f46febc8f
Felipe Figueiredo <philsf79@gmail.com>
2020-08-12 15:53:27 -0300

    indentation for better code review

--- scripts/results.R
+++ scripts/results.R
@@ -1,8 +1,11 @@
 source("scripts/input.R")
 library(pander)
 
-model.treat.t98strain <- aov(AmB ~ Treatment + Strain, data = df[CLine == "T98G"])
 # model.treat.t98strain <- aov(Absorbance ~ Treatment + Strain, data = df[CLine == "T98G"])
+model.treat.t98strain <- aov(
+  AmB ~ Treatment + Strain,
+  data = df[CLine == "T98G"]
+  )
 
 summary(model.treat.t98strain)
 # pander(model.treat.t98strain)
